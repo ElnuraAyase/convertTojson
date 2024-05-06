a1bc4d271e2794b0e46ae012fe91c573a52356bd
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-05-06 06:26:46 -0700

    Update classes.py

--- classes.py
+++ classes.py
@@ -10,5 +10,19 @@ class Person:
 class Car:
     def __init__(self, make, model, year, owner):
         self.make, self.model, self.year, self.owner = make, model, year,   owner
+# relating
+address = Address("123 Main St", " Cityville", "12345")
+person = Person("John", 30, address)
+car = Car("Toyota", "Camry", 2022,  person)
 
+# convert to jsonn
+address_json = json.dumps(address.__dict__)
+person_json = json.dumps(person.__dict__)
+car_json = json.dumps(car.__dict__)
+
+print("Address JSON:", address_json)
+
+print("Person JSON:", person_json)
+
+print("Car JSON: ", car_json)
 
